f51d7834118b411ece8476717bfc2b89c2f0f851
Abhishek Parmar <helloworldviz@gmail.com>
2023-12-25 21:37:35 +0530

    more commits

--- index.js
+++ index.js
@@ -15,8 +15,10 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) 
+  {slideIndex = 1}
+  if (n < 1) 
+  {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
